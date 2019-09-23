2ce80f7206f20ca98e3c13148f99b8428716c985
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2019-09-23 22:04:32 +0200

    Practica 1 version Pedro

---
+++ permisosKVM.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Permisos para ejecutar el emulador de android studio
+# Hay que dar permisos de ejecucion al 'fichero'/'dispositivo' /dev/kvm
+
+target='/dev/kvm'
+mask='0755'
+
+if [ $(id -u) -ne 0 ]; then
+	# Necesitas permisos de administrador
+	echo 'Necesitas permisos de administrador (ejecutar con sudo)' &>2
+	exit 1
+fi
+
+chmod $mask $target
+if [ $? -eq 0 ]; then
+	# Todo ha ido bien
+	echo 'Todo ha ido bien'
+	echo 'Mucha suerte programando :D'
+	exit 0
+else
+	echo 'Ha habido algun problema, contacte con su administrador favorito.' &>2
+	exit 2
+fi
