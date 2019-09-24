85409a98da31a01b91464850fcf15b8929fe143c
Juan José <jtambo@live.com>
2019-09-24 18:52:00 +0200

    RadioButtons

---
+++ PrimProyect/app/src/main/java/com/example/firstapp/RadioButtons.java
@@ -0,0 +1,14 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RadioButtons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_buttons);
+    }
+}
