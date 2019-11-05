40ee1d7996c6bf352a45beec25dd8edbffbf9508
Pedro Tamargo <pedro.tamargo.allue@protonmail.com>
2019-11-05 18:14:18 +0100

    Parte 1 de Android

---
+++ pr3/Notepadv3/app/src/main/java/es/unizar/eina/notepadv3/NoteEdit.java
@@ -0,0 +1,91 @@
+package es.unizar.eina.notepadv3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NoteEdit extends AppCompatActivity {
+
+    private EditText mTitleText;
+    private EditText mBodyText;
+    private Long mRowId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_edit);
+        setTitle(R.string.edit_note);
+
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+
+        mRowId = null;
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String title = extras.getString(NotesDbAdapter.KEY_TITLE);
+            String body = extras.getString(NotesDbAdapter.KEY_BODY);
+            mRowId = extras.getLong(NotesDbAdapter.KEY_ROWID);
+
+            if (title != null) {
+                mTitleText.setText(title);
+            }
+            if (body != null) {
+                mBodyText.setText(body);
+            }
+        }
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+
+                bundle.putString(NotesDbAdapter.KEY_TITLE, mTitleText.getText().toString());
+                bundle.putString(NotesDbAdapter.KEY_BODY, mBodyText.getText().toString());
+                if (mRowId != null) {
+                    bundle.putLong(NotesDbAdapter.KEY_ROWID, mRowId);
+                }
+
+                Intent mIntent = new Intent();
+                mIntent.putExtras(bundle);
+                setResult(RESULT_OK, mIntent);
+                finish();
+            }
+
+        });
+    }
+
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_edit);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_note_edit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+}
---
+++ pr3/Notepadv3/app/src/main/java/es/unizar/eina/notepadv3/Notepadv3.java
@@ -0,0 +1,147 @@
+package es.unizar.eina.notepadv3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.database.Cursor;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+
+public class Notepadv3 extends AppCompatActivity {
+
+    private static final int ACTIVITY_CREATE=0;
+    private static final int ACTIVITY_EDIT=1;
+
+    private static final int INSERT_ID = Menu.FIRST;
+    private static final int DELETE_ID = Menu.FIRST + 1;
+    private static final int EDIT_ID = Menu.FIRST + 2;
+
+    private NotesDbAdapter mDbHelper;
+    private Cursor mNotesCursor;
+    private ListView mList;
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notepadv3);
+
+        mDbHelper = new NotesDbAdapter(this);
+        mDbHelper.open();
+        mList = (ListView)findViewById(R.id.list);
+        fillData();
+
+        registerForContextMenu(mList);
+
+    }
+
+    private void fillData() {
+        // Get all of the notes from the database and create the item list
+        mNotesCursor = mDbHelper.fetchAllNotes();
+        startManagingCursor(mNotesCursor);
+
+        // Create an array to specify the fields we want to display in the list (only TITLE)
+        String[] from = new String[] { NotesDbAdapter.KEY_TITLE };
+
+        // and an array of the fields we want to bind those fields to (in this case just text1)
+        int[] to = new int[] { R.id.text1 };
+
+        // Now create an array adapter and set it to display using our row
+        SimpleCursorAdapter notes =
+                new SimpleCursorAdapter(this, R.layout.notes_row, mNotesCursor, from, to);
+        mList.setAdapter(notes);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(Menu.NONE, INSERT_ID, Menu.NONE, R.string.menu_insert);
+        return result;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case INSERT_ID:
+                createNote();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(Menu.NONE, DELETE_ID, Menu.NONE, R.string.menu_delete);
+        menu.add(Menu.NONE, EDIT_ID, Menu.NONE, R.string.menu_edit);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case DELETE_ID:
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                mDbHelper.deleteNote(info.id);
+                fillData();
+                return true;
+            case EDIT_ID:
+                info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                editNote(info.position, info.id);
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void createNote() {
+        Intent i = new Intent(this, NoteEdit.class);
+        startActivityForResult(i, ACTIVITY_CREATE);
+    }
+
+
+    protected void editNote(int position, long id) {
+        Cursor c = mNotesCursor;
+        c.moveToPosition(position);
+        Intent i = new Intent(this, NoteEdit.class);
+        i.putExtra(NotesDbAdapter.KEY_ROWID, id);
+        i.putExtra(NotesDbAdapter.KEY_TITLE, c.getString(
+                c.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE)));
+        i.putExtra(NotesDbAdapter.KEY_BODY, c.getString(
+                c.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
+        startActivityForResult(i, ACTIVITY_EDIT);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        Bundle extras = intent.getExtras();
+        switch(requestCode) {
+            case ACTIVITY_CREATE:
+                String title = extras.getString(NotesDbAdapter.KEY_TITLE);
+                String body = extras.getString(NotesDbAdapter.KEY_BODY);
+                mDbHelper.createNote(title, body);
+                fillData();
+                break;
+            case ACTIVITY_EDIT:
+                Long rowId = extras.getLong(NotesDbAdapter.KEY_ROWID);
+                if (rowId != null) {
+                    String editTitle = extras.getString(NotesDbAdapter.KEY_TITLE);
+                    String editBody = extras.getString(NotesDbAdapter.KEY_BODY);
+                    mDbHelper.updateNote(rowId, editTitle, editBody);
+                }
+                fillData();
+                break;
+        }
+    }
+
+}
